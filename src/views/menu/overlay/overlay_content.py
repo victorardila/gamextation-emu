5b665d9364913d61329a8364a03f0c215a57fd30
Valfonsoardila10 <victoradila@gmail.com>
2024-07-24 18:40:44 -0500

    Agregacion de logo de los creditos

--- src/views/menu/overlay/overlay_content.py
+++ src/views/menu/overlay/overlay_content.py
@@ -2,14 +2,15 @@ from PyQt5.QtWidgets import QWidget
 from PyQt5.QtCore import QSize, Qt, QTimer, pyqtSignal
 from PyQt5.uic import loadUi
 from qtawesome import icon
-from PyQt5.QtGui import QFont, QFontDatabase
+from PyQt5.QtGui import QFont, QFontDatabase, QPixmap, QPainter, QColor
 from PyQt5.QtSvg import QSvgWidget
 from datetime import datetime
 
 class OverlayContent(QWidget):
     theme_changed = pyqtSignal()  # Señal para indicar el cambio de tema
     menu_button_clicked = pyqtSignal(str)  # Señal para indicar que un botón del menú ha sido presionado
-
+    SVG_CREDITS = "src/assets/svg/icon.svg"
+    
     def __init__(self):
         super().__init__()
         self.init_main_menu()
@@ -83,6 +84,24 @@ class OverlayContent(QWidget):
             btn.style(image, QSize(250, 350), tooltip)
             btn.clicked.connect(lambda _, t=tooltip: self.emit_menu_button_clicked(t))  # Conectar clic a la señal
 
+    def apply_credits_styles(self):
+        self.logo.setPixmap(
+            self.colorize_svg(self.SVG_CREDITS, QColor('white'), QSize(80, 80))
+        )
+        
+    def colorize_svg(self, svg_path, color, size):
+        """Colorize an SVG file and return it as a QPixmap."""
+        pixmap = QPixmap(svg_path)
+        pixmap = pixmap.scaled(size, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+
+        # Apply color using QPainter
+        painter = QPainter(pixmap)
+        painter.setCompositionMode(QPainter.CompositionMode_SourceIn)
+        painter.fillRect(pixmap.rect(), color)
+        painter.end()
+
+        return pixmap
+    
     def emit_theme_change(self):
         self.theme_changed.emit()
 
